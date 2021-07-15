0ceed1ff8b54aa3129eeb8e4aa6393130e4824e2
elvisdias <elvisnaopresley@gmail.com>
2021-07-14 22:11:36 -0300

    minor text adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ class IndexPage extends React.Component {
                   <h4>Hello, </h4> 
                   <h4>I'm a 26 years old computer engineer by formation, currently working as a <span class="highlight">data scientist/software engineer</span> and optimizing myself to learn and express anything I gravitate to. </h4>
                   <h4>
-                  This is my home and thus has been thought out to reflect whatever I feel like putting out. For my playground I should be at <Link class='lab' to='/laboratory'>my laboratory</Link> anytime. Also, I'm kind of active on the social medias below.
+                  This is my home and thus has been thought out to reflect whatever I feel like putting out there. For my playground I should be at <Link class='lab' to='/laboratory'>my laboratory</Link> anytime.
                   </h4>
               </div>        
           </Layout>
@@ -58,7 +58,7 @@ class IndexPage extends React.Component {
                 <h4>Olá, </h4> 
                 <h4>Sou um engenheiro de computação por formação, tenho 26 anos, atualmente trabalhando como <span class="highlight">cientista de dados/engenheiro de software</span> e me otimizando para conseguir aprender e expressar qualquer coisa que eu me sinta atraído. </h4>
                 <h4>
-                 Esse é meu lar, e assim, foi pensado para refletir qualquer coisa que eu sinta que deva colocar para o mundo. Para experimentações eu devo estar no <Link class='lab' to='/laboratory'>meu laboratório</Link> a qualquer momento. Também, sou meio que ativo nas redes sociais abaixo. 
+                 Esse é meu lar, e assim, foi pensado para refletir qualquer coisa que eu sinta que deva colocar para o mundo. Para experimentações eu devo estar no <Link class='lab' to='/laboratory'>meu laboratório</Link> a qualquer momento.
                 </h4>
             </div>        
         </Layout>
